5f86d94c5b67dcc1bfd1d3816f7527140040a062
Artee <kznartee16reg@gmail.com>
2024-05-01 18:51:14 +0300

    ferst start bot

---
+++ mine.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config_data.config import Config, load_config
+from handlers.handlers import router
+
+config: Config = load_config()
+
+async def main():
+    bot = Bot(token=config.tg_bot.token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
