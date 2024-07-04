eb85e4c7b898a4ab2835eb4d6ef9d24c74eb1509
Artee <kznartee16reg@gmail.com>
2024-07-04 23:06:50 +0300

    TODO KB

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -3,11 +3,24 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 
+from keyboards import kb
+from lexicon import text_for_bot
+
 router = Router()
+
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
+    await msg.answer(
+        text_for_bot.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu
+    )
+
+
+@router.message(F.text == "Меню")
+@router.message(F.text == "Выйти в меню")
+@router.message(F.text == "◀️ Выйти в меню")
+async def menu(msg: Message):
+    await msg.answer(text_for_bot.menu, reply_markup=kb.menu)
 
 
 @router.message()
---
+++ keyboards/kb.py
@@ -0,0 +1,17 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, # ReplyKeyboardRemove узнать нахуй я это юзал
+
+# TO DO нада сделать кнопки, которые будут отправлять в бд даные о лавэ по классофикацуии.
+
+menu = [
+    [InlineKeyboardButton(text="📝 Генерировать текст", callback_data="generate_text"),
+    InlineKeyboardButton(text="🖼 Генерировать изображение", callback_data="generate_image")],
+    [InlineKeyboardButton(text="💳 Купить токены", callback_data="buy_tokens"),
+    InlineKeyboardButton(text="💰 Баланс", callback_data="balance")],
+    [InlineKeyboardButton(text="💎 Партнёрская программа", callback_data="ref"),
+    InlineKeyboardButton(text="🎁 Бесплатные токены", callback_data="free_tokens")],
+    [InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
+]
+
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
