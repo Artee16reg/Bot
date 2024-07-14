4df50a103c50341c397d0194ebcd23ebce1203fd
Artee <kznartee16reg@gmail.com>
2024-07-14 18:46:11 +0300

    TODO Kb

--- keyboards/kb.py
+++ keyboards/kb.py
@@ -1,16 +1,14 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, # ReplyKeyboardRemove узнать нахуй я это юзал
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup # ReplyKeyboardRemove узнать нахуй я это юзал
 
 # TO DO нада сделать кнопки, которые будут отправлять в бд даные о лавэ по классофикацуии.
 
 menu = [
-    [InlineKeyboardButton(text="📝 Генерировать текст", callback_data="generate_text"),
-    InlineKeyboardButton(text="🖼 Генерировать изображение", callback_data="generate_image")],
-    [InlineKeyboardButton(text="💳 Купить токены", callback_data="buy_tokens"),
-    InlineKeyboardButton(text="💰 Баланс", callback_data="balance")],
-    [InlineKeyboardButton(text="💎 Партнёрская программа", callback_data="ref"),
-    InlineKeyboardButton(text="🎁 Бесплатные токены", callback_data="free_tokens")],
-    [InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
-]
+    [InlineKeyboardButton(text="📝Записать доход", callback_data="generate_text"),
+    InlineKeyboardButton(text="📝 Записать расход", callback_data="generate_image"),],
+    
+    [InlineKeyboardButton(text="🖼 Посмотореть доход", callback_data="generate_text"),
+    InlineKeyboardButton(text="🖼 Посмотореть расход", callback_data="generate_image"),],
+    ]
 
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
